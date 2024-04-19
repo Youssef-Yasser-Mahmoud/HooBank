1dfa3edec1e4561f668034ea62b461fe624e5263
Youssef <youssefyaser2020@gmail.com>
2024-04-19 08:17:40 +0200

    initial

---
+++ src/Components/ReusableComponents/TextSide.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const TextSide = ({headerOne,headerTwo,paragraphOne,paragraphTwo,paragraphThree}) => {
+return (
+    <div className='text-container'>
+        <header>
+            <h3>{headerOne}</h3>
+            <h3>{headerTwo}</h3>
+        </header>
+        <div>
+            <p>{paragraphOne}</p>
+            <p>{paragraphTwo}</p>
+            <p>{paragraphThree}</p>
+        </div>
+    </div>
+)
+}
+
+export default TextSide
+
+ 
+
+
+
