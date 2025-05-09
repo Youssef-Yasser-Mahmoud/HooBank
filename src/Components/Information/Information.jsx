4b6188543e4724a1e51f353732f44a5f0ac1bd57
Youssef <youssefyaser2020@gmail.com>
2025-05-09 22:43:12 +0300

    Add Information component with styling

---
+++ src/Components/Information/Information.jsx
@@ -0,0 +1,24 @@
+import './Information.css';
+
+const Information = () => {
+  return (
+    <div className='bar-container'>
+      <div>
+        <p>3800+</p>
+        <p>User Active</p>
+        <span></span>
+      </div>
+      <div>
+        <p>230+</p>
+        <p>TRUSTED BY COMPANY</p>
+        <span></span>
+      </div>
+      <div>
+        <p>$230M+</p>
+        <p>TRANSACTION</p>
+      </div>
+    </div>
+  );
+};
+
+export default Information;
